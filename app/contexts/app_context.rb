6e47a18c27b67174ff5260f0e5005caada5250bb
Howard Chen <hchen7913@gmail.com>
2020-05-27 15:44:14 -0700

    Add comments

--- app/contexts/app_context.rb
+++ app/contexts/app_context.rb
@@ -1,5 +1,3 @@
-# This class is responsible for fetching and organizing
-# data about apps in a coherent Hash that can be sent to the frontend
 SELECT_FAVS = " user_favorite_apps.user_id as follower_id, user_favorite_apps.app_id as favapp_id"
 SELECT_RATINGS = " ratings.user_id as reviewer_id, ratings.app_id as reviewed_app_id, ratings.score"
 SELECT = " apps.*, #{SELECT_FAVS}, #{SELECT_RATINGS}"
@@ -17,9 +15,18 @@ Join = -> (params) {
 }
 
 
+# This class is responsible for fetching and organizing
+# data about apps in a coherent Hash that can be sent to the frontend
+# If the user is logged in, a :score and :is_favorite property are attached to each Hash
+# If the user is not logged in, :score and :is_favorite default to 0
+# 
+# In all cases, a :ratings property is attached to the hash with the following form:
+#
+# { 1 => count_1, 2 => count_2, 3 => count_3, 4 => count_4, 5 => count_5 }
+#
+# This allows the frontend to calculate the rating distribution for a particular app.
 class AppContext
-    # Simple function that fetches all apps, and if the current user is logged in, attaches a 'is_favorite'
-    # tag to the hash
+
     def self.all (params = {})
         if params[:current_user]
             apps = App.joins(Join.call(params)).select(SELECT)
@@ -94,12 +101,14 @@ class AppContext
         aggregated_stats
     end
 
+    # Attaches :ratings property to an array of apps
     def self.with_ratings(apps, rating_stats)
         apps.map { | app | 
             self.with_rating(app, rating_stats)
         }
     end
 
+    # Attaches :ratings property to a single app
     def self.with_rating(app, rating_stats)
         app["ratings"] = self.empty_ratings
         app_ratings = rating_stats[app["id"]]
