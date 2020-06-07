83d5ab65f57d9fb6d2b034e7c699a35cdf36891d
Howard Chen <hchen7913@gmail.com>
2020-06-07 10:30:15 -0700

    Add tests for the AppContext

--- test/models/app_test.rb
+++ test/models/app_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class AppTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "an app with no ratings is calculated to have 0 ratings from 1 to 5" do
+    app = apps(:one)
+    result = AppContext.find_by(id: app.id)
+    expected = {
+      1 => 0,
+      2 => 0,
+      3 => 0,
+      4 => 0,
+      5 => 0,
+    }
+    assert_equal(expected, result["ratings"])
+  end
+
+  test "an app with no ratings from it's specified `logged-in` user is given a user-rating of 0" do 
+    app = apps(:one)
+    user = users(:one)
+    result = AppContext.find_by(id: app.id, current_user: user)
+    expected = 0
+    assert_equal(expected, result["score"])
+  end
 end
